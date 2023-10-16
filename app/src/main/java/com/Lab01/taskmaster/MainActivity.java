9d501efb4fc14eb3b53dac572689a9c24a4c11fe
Farah Elaydi <98386953+farahaydi@users.noreply.github.com>
2023-10-16 12:28:02 +0300

    testTaskMaster

---
+++ app/src/main/java/com/Lab01/taskmaster/MainActivity.java
@@ -0,0 +1,14 @@
+package com.Lab01.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
